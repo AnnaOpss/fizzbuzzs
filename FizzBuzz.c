7619ecc5b39569621c019f73fdf32819dc373438
RobClap <robclap8@gmail.com>
2016-05-27 08:57:13 +0200

    Added README and BASH fizzbuzz

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -4,16 +4,13 @@ int main (int argc, char* argv[])
 {
 	int i;
 	for (i=1; i<=MAX; i++) {
-		if (i%3 == 0 && i%5==0)
-			printf ("FizzBuzz\n");
-		else
-			if (i%3 == 0)
-				printf ("Fizz\n");
-			else
-				if (i%5 == 0)
-					printf ("Buzz\n");
-				else
-					printf ("%d\n", i);
+		if (i%3 == 0)
+			printf("Fizz");
+		if (i%5 == 0)
+			printf("Buzz");
+		if (i%5 != 0 && i%3 != 0)
+			printf("%d", i);
+		printf("\n");
 	}
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fizzbuzzs
+This is a collection of implementations for the fizzbuzz or cracklepop algorithm.
+
+Each source contains a readable and simple implementation and a terrible, almost unreadable version of the same solution.
+
+The fizzbuzz algorithm is described [here](https://en.wikipedia.org/wiki/Fizz_buzz).
