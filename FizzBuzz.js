31183aa97f5a89a1a2338e3b04ac0994ea63e191
AnnaOpss <hi@anna.so>
2016-05-31 18:34:45 +0200

    Beautified FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,7 +1,24 @@
 function fizzbuzz(n) {
-	console.log(i%3 === 0 && i%5 == 0 ? "FizzBuzz": i%3 === 0 ? "Fizz" : i%5 === 0 ? "Buzz" : i)
+	console.log(i%3 === 0 && i%5 == 0 ? "FizzBuzz": i%3 === 0 ? "Fizz" : i%5 === 0 ? "Buzz" : i);
+}
+
+function betterFizzbuzz(n) {
+	var result = "";
+	if (n % 3 === 0)
+		result += "Fizz";
+	if (n % 5 === 0)
+		result += "Buzz";
+	if (result === "")
+		result = n;
+	console.log(result);
 }
 
 for(var i = 0; i < 100; i++){
+	fizzbuzz(i);
+}
 
+console.log();
+
+for(var i = 0; i < 100; i++){
+	betterFizzbuzz(i);
 }
